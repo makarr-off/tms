25aacc93d3a7b64d7c8766b63f98bc423e0df7de
makarr-off <makarr-off@yandex.ru>
2020-03-30 19:58:41 +0300

    version 0.0.8

--- src/project/urls.py
+++ src/project/urls.py
@@ -7,13 +7,19 @@ from django.urls import path
 here = Path(__file__).parent.resolve()
 
 
-def view(r):
+def view_name(r):
     index = here.parent.parent / "index.html"
     with index.open() as f:
         return HttpResponse(f.read())
 
+def view_picture(u):
+    picture = here.parent.parent / "img.png"
+    with picture.open("rb") as f:
+        return HttpResponse(f.read(), content_type="image/jpeg")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', view),
+    path('my_name/', view_name),
+    path('me/', view_picture)
 ]
