32306990e3a73225c91e4291f0f67b80eb2e85b1
makarr-off <makarr-off@yandex.ru>
2020-04-02 12:00:51 +0300

    version 0.1.1

--- src/project/urls.py
+++ src/project/urls.py
@@ -8,22 +8,34 @@ here = Path(__file__).parent.resolve()
 
 
 def read_static(file, content_type):
-    with file.open() as f:
+    with file.open("rb") as f:
         return HttpResponse(f.read(), content_type)
 
 
 def view_list(r):
-    name = here.parent.parent / "index.html"
-    return read_static(name, content_type=None)
+#    name = here.parent.parent / "index.html"
+    return read_static(here.parent.parent / "index.html", content_type=None)
 
 
-def view_picture(u):
-    name = here.parent.parent / "img1.png"
-    return read_static(name, content_type="image/jpeg")
+def view_picture(r):
+#    name = here.parent.parent / "DSC_0031.png"
+    return read_static(here.parent.parent / "DSC_0031.png", content_type="image/jpeg")
+
+
+def view_picture2(r):
+#    name = here.parent.parent / "DSC_0031.png"
+    return read_static(here.parent.parent / "our_family.png", content_type="image/jpeg")
+
+
+def view_resume(r):
+#    name = here.parent.parent / "DSC_0031.png"
+    return read_static(here.parent.parent / "Resume.html", content_type=None)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', view_list),
-    path('img1/', view_picture),
+    path('clone/', view_picture),
+    path('Resume/', view_resume),
+    path('our_family/', view_picture2),
 ]
