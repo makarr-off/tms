8f1b5eea36a768e5508a15dc5fd58dfb930cf4de
makarr-off <makarr-off@yandex.ru>
2020-05-22 13:07:31 +0300

    Fixed settings

--- src/project/settings.py
+++ src/project/settings.py
@@ -21,7 +21,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "applications.onboarding.apps.OnboardingConfig",
     "apps.index",
     "apps.resume",
 ]
