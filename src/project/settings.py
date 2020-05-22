562984dc02563e0e7fd763a4317b27df4023cc06
makarr-off <makarr-off@yandex.ru>
2020-05-22 13:02:07 +0300

    Added Dockerfile

--- src/project/settings.py
+++ src/project/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "applications.onboarding.apps.OnboardingConfig",
     "apps.index",
     "apps.resume",
 ]
